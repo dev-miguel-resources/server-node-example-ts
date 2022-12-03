c143d28bc6a13083ff07e7bf26e7465f41b02e6f
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-12-03 08:39:54 -0300

    test example

--- index.js
+++
@@ -1,1 +0,0 @@
-console.log("Starting server Node.js with JS");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Starting server Node.js with TS");
+console.log('Starting server Node.js with TS')
+console.log('asassasassa')
+
+               interface Medic {
+      name: string
+   codMed: number
+}
+
+const medic: Medic = { name: 'Miguel', codMed: 14 }
+console.log(medic)
